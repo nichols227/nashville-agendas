64cea25d0be4f01004594d592c5dc1afa1a32176
Nick Scoliard <nscoliard@google.com>
2023-11-24 09:59:21 -0500

    Add Murfreesboro

--- index.py
+++ index.py
@@ -5,6 +5,10 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
+def filterRows(row):
+        today = datetime.now()
+        return row['agenda'] and ((today - timedelta(days=7)) <= row['date'] <= today)
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -21,11 +25,28 @@ def davidson():
         name = tds[0].text
         dateString = "{} {}".format(tds[1].text, tds[3].text.replace("\n", ""))
         dateFormat = '%m/%d/%Y %I:%M %p'
-        date = datetime.strptime(dateString, '%m/%d/%Y %I:%M %p')
+        date = datetime.strptime(dateString, dateFormat)
         agenda = tds[7].find('a').get('href')
         return {"name": name, "date": date, "dateString": date.strftime(dateFormat), "agenda": agenda}
-    def filterRows(row):
-        today = datetime.now()
-        return row['agenda'] and ((today - timedelta(days=7)) <= row['date'] <= today)
+    
     agendaList = filter(filterRows, map(createRowObject, rows))
     return render_template('davidson.html', agendaList=agendaList)
+
+@app.route("/murfreesboro")
+def murfreesboro():
+    source = requests.get('https://www.murfreesborotn.gov/agendacenter').text
+    soup = BeautifulSoup(source, 'html.parser')
+    def murfreesboro_rows(css_class):
+        return css_class == "catAgendaRow"
+    rows = soup.find_all('tr', class_=murfreesboro_rows)
+    def createRowObject(row):
+        tds = row.find_all('td')
+        name = tds[0].p.text
+        dateString = tds[0].h4.strong.text
+        dateFormat = '%b %d, %Y'
+        date = datetime.strptime(dateString, dateFormat)
+        agenda = tds[-1].find('a', class_="pdf").get('href')
+        council = row.find_parent('div').h2.text[1:]
+        return {"name": name, "date": date, "dateString": date.strftime(dateFormat), "agenda": agenda, "council": council}
+    agendaList = filter(filterRows, map(createRowObject, rows))
+    return render_template('murfreesboro.html', agendaList=agendaList)
